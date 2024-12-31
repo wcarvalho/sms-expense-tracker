a63bf78ae1ec14ddb9063df7af03a6f21ed103bd
Wilka Carvalho <wcarvalho92@gmail.com>
2024-12-31 10:41:43 -0500

    added email webhook

--- netlify/functions/email-webhook.js
+++ netlify/functions/email-webhook.js
@@ -41,7 +41,7 @@ exports.handler = async (event, context) => {
     return {
       statusCode: 200,
       body: 'Email processed',
-      headers: { 'Content-Type': 'text/plain' }¸
+      headers: { 'Content-Type': 'text/plain' }
     };
 
   } catch (error) {
